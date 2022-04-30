c968302d564ad134a4bd778400e4c38245cbdaf7
aquilesics <aquiles.cout@gmail.com>
2022-04-30 14:21:17 -0300

    ajustes geraios

--- pages/index.vue
+++ pages/index.vue
@@ -36,7 +36,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
             </svg>
-            <p>+55 11 98290-7503</p>
+            <p>+55 11 98290-7503 (Whatsapp)</p>
           </div>
           <div class="flex space-x-4 items-center">
             <svg
@@ -117,7 +117,7 @@ export default {
         { title: 'Git', items: ['Commits, Push, Branch'], image: 'git.svg' },
         { title: 'Java Script (básico)', items: ['Node', 'Noções de Funções e objetos'], image: 'js.svg' },
         { title: 'css', items: ['Responsive Design, Flex, Grid', 'Animations etc'], image: 'css.svg' },
-        { title: 'vue (básico)', items: ['Single components', 'Templates, diretivas, props'], image: 'vue.svg' },
+        { title: 'Vue (básico)', items: ['Single components', 'Templates, diretivas, props'], image: 'vue.svg' },
         { title: 'Power bi (Intermediário)', items: ['Dax e Formula M', 'Integração com Views'], image: 'powerbi.svg' },
         { title: 'Postgresql (básico)', items: ['DML, Views', 'Joins e Procedures', 'Funções personalizadas'], image: 'postgres.svg' }
       ],
